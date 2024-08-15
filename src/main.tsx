ab787015cb60a39e1e3168b4fe533f82a13677bc
mspavanelli <matheus.pavanelli@madeinweb.app>
2024-08-15 16:59:15 -0300

    chore: setup tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import './index.css'
+import './styles/global.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.tsx'
+import { App } from './app'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
